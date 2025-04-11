b8b9e8333af9e7c7001b8aa17c137bcd902e8d5d
Erjon Thaqi <e3cthe@e3ag.net>
2025-04-11 16:33:28 +0200

    password saved as hashed in DB

--- src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
+++ src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
@@ -2,6 +2,10 @@ package ch.bbw.pr.tresorbackend.service;
 
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * PasswordEncryptionService
  * @author Peter Rutschmann
@@ -14,8 +18,20 @@ public class PasswordEncryptionService {
       //todo anpassen!
    }
 
-   public String hashPassword(String password) {
-      //todo anpassen!
-      return password;
+   public String hashPassword(String password) throws Exception {
+      try {
+         MessageDigest digest = MessageDigest.getInstance("SHA-256");
+         byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+         StringBuilder hexString = new StringBuilder();
+         for (byte b : encodedHash) {
+            hexString.append(String.format("%02x", b));
+         }
+
+         return hexString.toString();
+      } catch (Exception e) {
+         //logger hinzufügen
+         throw new Exception("SHA-256 algorithm not found", e);
+      }
    }
 }
